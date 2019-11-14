3fec8db958fddd8e91687a0259a566af8a6747d1
Matheus Rodrigues Guedes <48195358+MatheusRGuedes@users.noreply.github.com>
2019-11-14 11:16:03 -0300

    updating improviments

--- index.js
+++ index.js
@@ -1,27 +1,37 @@
 console.log('Testando..');
 
-var field = document.getElementById('res');
-field.innerHTML = 0;
-
 const soma = document.getElementById('soma'),
 	  sub = document.getElementById('sub'),
 	  mult = document.getElementById('mult'),
 	  div = document.getElementById('div'),
-	  buttons = document.querySelectorAll('.button');
+	  buttons = document.querySelectorAll('.button'),
+	  back = document.getElementsByClassName('back')[0],
+	  clr = document.querySelector('.clear');
 
-let inserted = 0;
+let inserted = 0,
+	field = document.getElementById('res'),
+	fontSize = 25;
+
+field.innerHTML = 0;
 
 for ( let pos = 0; pos < buttons.length; pos++ ) {
 	buttons[pos].addEventListener('click', function() {
 
 		let digitedValue = buttons[pos].value,
-			values = ["-", "+", "*", "/"],
+			values = [ "-", "+", "*", "/" ],
 			conta = field.innerHTML;
 
 		//verifica o primeiro digito 
 		if ( conta.length == 1 )
 			field.innerHTML = ' ';
 
+		//diminui a fonte qnto maior for o res
+		if ( conta.length > 6 && conta.length % 2) {
+			fontSize -= 1.5;
+			document.getElementById('res').style.fontSize = fontSize + "px";
+		}
+
+		//identifica o primeiro operador selecionado
 		if ( values.includes( digitedValue ) && inserted == 0 ) {
 			inserted = 1;
 			field.innerHTML += digitedValue;
@@ -33,6 +43,7 @@ for ( let pos = 0; pos < buttons.length; pos++ ) {
 			field.innerHTML += digitedValue;
 		}
 
+		//troca o operador para o atual 
 		else if ( values.includes(digitedValue) && digitedValue != conta.charAt( conta.length - 1 ) ) {
 			field.innerHTML = conta.substring(0, conta.length - 1) + digitedValue; 
 			//alert(conta.charAt( conta.length - 1 ));
@@ -42,17 +53,20 @@ for ( let pos = 0; pos < buttons.length; pos++ ) {
 
 const calc = document.querySelector('.calc');
 calc.addEventListener('click', function () {
-	var conta = field.innerHTML;
+	let conta = field.innerHTML;
 	field.innerHTML = eval(conta);
 });
 
 
-const clr = document.querySelector('.clear');
-
 function clear () {
 	document.getElementById('res').textContent = "";
 }
-
 clr.onclick = function() { clear() };
+
+function backValue () {
+	let conta = field.innerHTML;
+	field.innerHTML = conta.substring( 0, conta.length - 1 );
+} 
+back.onclick = function() { backValue() };
 
 //debugger; pausa a execução
