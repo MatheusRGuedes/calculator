615a88d73fe642f674c379b3b697d6a77cbc1552
Matheus Rodrigues Guedes <48195358+MatheusRGuedes@users.noreply.github.com>
2019-12-24 13:57:58 -0300

    update js
    adding a code improvement

--- index.js
+++ index.js
@@ -1,72 +1,104 @@
-console.log('Testando..');
+console.log('Ainda faltam melhorias que quero implementar :)');
 
-const soma = document.getElementById('soma'),
-	  sub = document.getElementById('sub'),
-	  mult = document.getElementById('mult'),
-	  div = document.getElementById('div'),
-	  buttons = document.querySelectorAll('.button'),
+const buttons = document.querySelectorAll('.button'),
 	  back = document.getElementsByClassName('back')[0],
-	  clr = document.querySelector('.clear');
+	  clr = document.querySelector('.clear'),
+	  field = document.getElementById('res');
 
-let inserted = 0,
-	field = document.getElementById('res'),
+//criar botao para adicionar vírgula
+
+let operators = [ '-', '+', '*', '/', ',' ],
+	inserted = 0,
 	fontSize = 25;
 
-field.innerHTML = 0;
+field.innerHTML = '0';
 
-for ( let pos = 0; pos < buttons.length; pos++ ) {
-	buttons[pos].addEventListener('click', function() {
+function insertValue( value ) {
+	var conta = field.innerHTML;
 
-		let digitedValue = buttons[pos].value,
-			values = [ "-", "+", "*", "/" ],
-			conta = field.innerHTML;
+	//verifica o primeiro digito
+	if ( field.innerText.length == 1 && conta === '0' ) {
+		field.innerHTML = '';
+	}
 
-		//verifica o primeiro digito 
-		if ( conta.length == 1 )
-			field.innerHTML = ' ';
+	//diminui a fonte qnto maior for o res
+	if ( conta.length > 6 && conta.length % 2) {
+		fontSize -= 1.5;
+		document.getElementById('res').style.fontSize = fontSize + "px";
+	}
 
-		//diminui a fonte qnto maior for o res
-		if ( conta.length > 6 && conta.length % 2) {
-			fontSize -= 1.5;
-			document.getElementById('res').style.fontSize = fontSize + "px";
-		}
+	//identifica o primeiro operador selecionado
+	if ( operators.includes( value ) && inserted == 0 ) {
+		inserted = 1;
+		field.innerHTML += value;
+	}
 
-		//identifica o primeiro operador selecionado
-		if ( values.includes( digitedValue ) && inserted == 0 ) {
-			inserted = 1;
-			field.innerHTML += digitedValue;
-		}
+	//não repete o operador
+	else if ( inserted != 1 || ! operators.includes( value ) ) {
+		inserted = 0;
+		field.innerHTML += value;
+	}
 
-		//não repete o operador
-		else if ( inserted != 1 || ! values.includes( digitedValue ) ) {
-			inserted = 0;
-			field.innerHTML += digitedValue;
-		}
-
-		//troca o operador para o atual 
-		else if ( values.includes(digitedValue) && digitedValue != conta.charAt( conta.length - 1 ) ) {
-			field.innerHTML = conta.substring(0, conta.length - 1) + digitedValue; 
-			//alert(conta.charAt( conta.length - 1 ));
-		}
-	});
+	//troca o operador para o atual
+	else if ( operators.includes(value) && value != conta.charAt( conta.length - 1 ) ) {
+		field.innerHTML = conta.substring(0, conta.length - 1) + value; 
+		//alert(conta.charAt( conta.length - 1 ));
+	}
 }
 
-const calc = document.querySelector('.calc');
-calc.addEventListener('click', function () {
-	let conta = field.innerHTML;
-	field.innerHTML = eval(conta);
-});
+//nome clear apenas, nao pode é uma função existente
+function clearRegister () {
 
+	document.getElementById('res').innerHTML = '0';
 
-function clear () {
-	document.getElementById('res').textContent = "";
-}
-clr.onclick = function() { clear() };
+};
+
 
 function backValue () {
 	let conta = field.innerHTML;
-	field.innerHTML = conta.substring( 0, conta.length - 1 );
-} 
-back.onclick = function() { backValue() };
 
-//debugger; pausa a execução
+	if ( conta && conta.length > 1 ) {
+
+		let conta = field.innerHTML;
+		field.innerHTML = conta.substring( 0, conta.length - 1 );
+
+	} else if ( conta && conta.length == 1 ) {
+
+		field.innerHTML = '0';
+
+	}
+};
+
+
+function calc() {
+	if ( field.innerHTML ) {
+
+		//troca a virgula por ponto e faz a conta
+		let conta = eval( field.innerHTML.replace(',', '.') );
+
+		field.innerHTML = addVirgula( conta.toString() ); //converte para string o res do eval
+
+	}
+};
+
+
+const calculate = document.querySelector('.calc');
+calculate.onclick = function() { calc() };
+
+
+function addVirgula ( str ) {
+	//debugger
+	let virgula = false, 
+		count;
+
+	//sem advirgula( valor.toString() ) da undefined na propriedade length
+	for ( count = 0; count < str.length; count++ ) {
+		if ( str[count] === "." ) {
+			virgula = true;
+			break;
+		}
+	}
+
+	if ( virgula == false ) return str;
+	else return str.replace('.', ',');
+};
